486198d362a8226d3f9f0bdd6e40960cff19047b
Hackerart512 <hackerart512@gmail.com>
2026-04-08 21:48:54 +0530

    fixed issue

--- src/pages/portfolio/MonisaErpPortal.jsx
+++ src/pages/portfolio/MonisaErpPortal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Navbar from '../../components/common/Navbar'
 import Footer from '../../components/common/Footer'
